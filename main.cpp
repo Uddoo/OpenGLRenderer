8dd4091ef93babc5dde8222857567b38fa752112
PureDeep <542057950@qq.com>
2023-03-19 22:45:39 +0800

    perf : point light + main

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ Camera camera; // 相机
 
 Texture brickTexture;
 Texture dirtTexture;
+Texture plainTexture;
 
 Material shinyMaterial; // 高光材质
 Material dullMaterial; // 低光材质
@@ -97,6 +98,18 @@ void CreateObjects()
 		0.0f, 1.0f, 0.0f,    0.5f, 1.0f,   0.0f, 0.0f, 0.0f
 	};
 
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		0, 3, 2
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,   0.0f, 0.0f,   0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,   10.0f, 0.0f,   0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,   10.0f, 10.0f,   0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,   0.0f, 10.0f,   0.0f, -1.0f, 0.0f
+	};
+
 	CalcAverageNormals(indices, 12, vertices, 32, 8, 5);
 
 	Mesh* obj1 = new Mesh();
@@ -106,6 +119,10 @@ void CreateObjects()
 	Mesh* obj2 = new Mesh();
 	obj2->CreateMesh(vertices, indices, 32, 12);
 	meshList.push_back(obj2);
+
+	Mesh* obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
 }
 
 void CreateShaders()
@@ -129,8 +146,10 @@ int main()
 	brickTexture.LoadTexture(); // 加载纹理
 	dirtTexture = Texture("Textures/dirt.png");
 	dirtTexture.LoadTexture();
+	plainTexture = Texture("Textures/plain.png");
+	plainTexture.LoadTexture();
 
-	shinyMaterial = Material(1.0f, 32);
+	shinyMaterial = Material(1.0f, 256);
 	dullMaterial = Material(0.3f, 4);
 
 	mainLight = DirectionalLight(1.0f, 1.0f, 1.0f,
@@ -207,6 +226,14 @@ int main()
 		dirtTexture.UseTexture();
 		dullMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess); // 使用低光材质
 		meshList[1]->RenderMesh();
+
+		model = glm::mat4(1.0f); // 重置模型矩阵
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f)); // 平移
+		//model = glm::scale(model, glm::vec3(0.4, 0.4, 1.0f)); // 缩放
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model)); // 设置 uniform 变量 model
+		plainTexture.UseTexture();
+		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess); // 使用低光材质
+		meshList[2]->RenderMesh();
 
 		glUseProgram(0); // 不使用着色器
 
